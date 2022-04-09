74426fdf7927d186c1abf0045ff205b1d87c77c6
serpak <zvinarevski946@yandex.com>
2022-04-09 23:34:02 +0300

    Добавьте файлы проекта.

---
+++ Lab 6/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string num_s = "";
+            char[] c = s.ToCharArray();
+            foreach (char ch in c)
+            {
+                if (char.IsNumber(ch))
+                {
+                    num_s += ch + " ";
+                }
+            }
+            num_s = num_s.Trim();
+            label1.Text = num_s;
+        }
+    }
+}
